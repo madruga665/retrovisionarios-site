1f748d53685b0e9be0942a06b9768e837bf4b9ad
madruga665 <madruga665@hotmail.com>
2024-10-28 22:01:13 -0300

    refactor: cria um array para colocar o conteudo dos botões

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,23 @@ import { ProfileImage } from '@/components/profile-image/profileImage';
 import { Calendar } from '@/components/calendar/calendar'
 
 export default function Home() {
+  const buttonsContent = [
+    {
+      url:"https://www.youtube.com/@Retrovisionarios1986",
+      icon:"/images/icons/youtube.svg",
+      label:"YouTube"
+    },
+    {
+      url:"https://www.instagram.com/retrovisionarios1986",
+      icon:"/images/icons/instagram.svg",
+      label:"Instagram"
+    },
+    {
+      url:"https://www.facebook.com/profile.php?id=61558180019563",
+      icon:"/images/icons/facebook.svg",
+      label:"Facebook"
+    }
+  ]
   return (
     <main className="px-2 sm:px-10">
       <div className="w-full gap-8 p-4 mt-10 flex flex-col items-center justify-center">
@@ -15,22 +32,12 @@ export default function Home() {
       </div>
 
       <div className="w-full py-8 flex flex-col items-center justify-center gap-8">
-        <LinkButton
-          url="https://www.youtube.com/@Retrovisionarios1986"
-          icon="/images/icons/youtube.svg"
-          label="YouTube"
-        />
-        <LinkButton
-          url="https://www.instagram.com/retrovisionarios1986"
-          icon="/images/icons/instagram.svg"
-          label="Instagram"
-        />
-        <LinkButton
-          url="https://www.facebook.com/profile.php?id=61558180019563"
-          icon="/images/icons/facebook.svg"
-          label="Facebook"
-        />
+        {buttonsContent.map(({url, icon, label}) => {
+          return <LinkButton url={url} icon={icon} label={label} />
+         })
+        }
       </div>
+
       <div className="w-full py-8 flex flex-col items-center justify-center gap-8">
         <Calendar />
       </div>
