267747b2d3b21c2c173915b86cc358ba7fcb8571
madruga665 <madruga665@hotmail.com>
2024-10-29 21:15:11 +0000

    chore: formata home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,19 +5,19 @@ import { Calendar } from '@/components/calendar/calendar'
 export default function Home() {
   const buttonsContent = [
     {
-      url:"https://www.youtube.com/@Retrovisionarios1986",
-      icon:"/images/icons/youtube.svg",
-      label:"YouTube"
+      url: "https://www.youtube.com/@Retrovisionarios1986",
+      icon: "/images/icons/youtube.svg",
+      label: "YouTube"
     },
     {
-      url:"https://www.instagram.com/retrovisionarios1986",
-      icon:"/images/icons/instagram.svg",
-      label:"Instagram"
+      url: "https://www.instagram.com/retrovisionarios1986",
+      icon: "/images/icons/instagram.svg",
+      label: "Instagram"
     },
     {
-      url:"https://www.facebook.com/profile.php?id=61558180019563",
-      icon:"/images/icons/facebook.svg",
-      label:"Facebook"
+      url: "https://www.facebook.com/profile.php?id=61558180019563",
+      icon: "/images/icons/facebook.svg",
+      label: "Facebook"
     }
   ];
 
@@ -33,7 +33,7 @@ export default function Home() {
       </div>
 
       <div className="w-full py-8 flex flex-col items-center justify-center gap-8">
-        {buttonsContent.map(({url, icon, label}) => {
+        {buttonsContent.map(({ url, icon, label }) => {
           return <LinkButton key={label} url={url} icon={icon} label={label} />
         })}
       </div>
