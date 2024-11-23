f41a5e317ec0b27c9963a43a4cee295db1a277d6
madruga665 <madruga665@hotmail.com>
2024-11-23 11:44:01 -0300

    add icone de calendario

--- src/components/calendar/calendar.tsx
+++ src/components/calendar/calendar.tsx
@@ -1,11 +1,19 @@
+import Image from 'next/image';
+
 export function Calendar() {
   return (
     <div className="flex flex-col gap-4">
       <h2 className="text-3xl font-bold">Agenda</h2>
-        <ul>
-          <li className="line-through">📆 17/11/24 - Vega Rock Day (Evento Fechado)</li>
-          <li>📆 08/12/24 - Moto Club Cães do Litoral</li>
-        </ul>
+      <ul>
+        <li className="flex gap-1 line-through">
+          <Image src="/images/icons/calendar.png" alt="Calendar icon" width={20} height={20} />
+          <p>17/11/24 - Vega Rock Day (Evento Fechado)</p>
+        </li>
+        <li className='flex gap-1'>
+          <Image src="/images/icons/calendar.png" alt="Calendar icon" width={20} height={20} />
+          <p>08/12/24 - Moto Club Cães do Litoral</p>
+        </li>
+      </ul>
     </div>
-  )
+  );
 }
