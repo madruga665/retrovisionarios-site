bbe9b5df43dd8d5214ea6eae3bdce1c2a912c8c3
madruga665 <madruga665@hotmail.com>
2024-11-23 11:31:38 -0300

    add evento cães do litoral

--- src/components/calendar/calendar.tsx
+++ src/components/calendar/calendar.tsx
@@ -3,7 +3,8 @@ export function Calendar() {
     <div className="flex flex-col gap-4">
       <h2 className="text-3xl font-bold">Agenda</h2>
         <ul>
-          <li>🗓 17/11/24 - Vega Rock Day (Evento Fechado)</li>
+          <li className="line-through">📆 17/11/24 - Vega Rock Day (Evento Fechado)</li>
+          <li>📆 08/12/24 - Moto Club Cães do Litoral</li>
         </ul>
     </div>
   )
