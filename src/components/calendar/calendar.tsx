b2248da7d3b86b0554bb0d8fb5489e897b978586
Luciano Amâncio <madruga665@hotmail.com>
2025-05-24 19:17:15 -0300

    atualização dos eventos

--- src/components/calendar/calendar.tsx
+++ src/components/calendar/calendar.tsx
@@ -4,20 +4,29 @@ export function Calendar() {
   type Event = {
     date: string;
     name: string;
-    isCompleted: boolean;
   };
 
+  function isCompleted(dateString: string): boolean {
+    const [day, month, year] = dateString.split('/').map(Number);
+    const fullYear = year < 100 ? 2000 + year : year;
+    const eventDate = new Date(fullYear, month - 1, day, 23, 59, 59);
+    const now = new Date();
+
+    return eventDate < now;
+  }
+
   const EventsList: Event[] = [
-    { date: '17/11/24', name: 'Vega Rock Day (Evento Fechado)', isCompleted: true },
-    { date: '08/12/24', name: 'Moto Club Cães do Litoral', isCompleted: true },
+    { date: '07/06/25', name: 'Jamel Studio' },
+    { date: '27/06/25', name: 'The Olds' },
+    { date: '12/07/25', name: 'Moto Club Ovelhas Desgarradas' },
   ];
 
   return (
     <div className="flex flex-col gap-4">
-      <h2 className="text-3xl font-bold">Agenda 2024</h2>
+      <h2 className="text-3xl font-bold">Agenda 2025</h2>
       <ul>
-        {EventsList.map(({ date, name, isCompleted }) => (
-          <Event date={date} name={name} key={date + name} isCompleted={isCompleted} />
+        {EventsList.map(({ date, name }) => (
+          <Event date={date} name={name} key={date + name} isCompleted={isCompleted(date)} />
         ))}
       </ul>
     </div>
