8eb1afa8f39b0199ffe89c5438b47d81bfb1b46c
Luciano Amâncio <madruga665@hotmail.com>
2025-11-19 16:18:55 -0300

    chore: config jest

---
+++ src/components/profile-image/__tests__/profileImage.test.tsx
@@ -0,0 +1,10 @@
+import { render, screen } from '@testing-library/react';
+import { ProfileImage } from '../profileImage';
+
+describe('ProfileImage', () => {
+  it('should render the profile image', () => {
+    render(<ProfileImage />);
+    const profileImage = screen.getByTestId('profile-image');
+    expect(profileImage).toBeInTheDocument();
+  });
+});
