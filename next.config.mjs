3ec427b71f07028872dda5f19c3b294ac6b09058
Luciano Amâncio <madruga665@hotmail.com>
2025-11-24 12:15:15 -0300

    chore: update ci node version

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    eslint: {
+  eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
 
   images: {
-remotePatterns: [
+    remotePatterns: [
       {
         protocol: 'https',
         hostname: process.env.NEXT_PUBLIC_IMAGES_HOST,
         pathname: '/**',
       },
-    ]
-  }
+    ],
+  },
 };
 
 export default nextConfig;
