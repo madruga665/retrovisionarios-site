23f0657af65250a859f28eedaca46864c3615898
madruga665 <madruga665@hotmail.com>
2026-01-24 02:27:09 -0300

    chore: use retrovisionarios-api to get all events

--- src/components/profile-image/profile-image.tsx
+++ src/components/profile-image/profile-image.tsx
@@ -1,8 +1,18 @@
+import Image from 'next/image';
+
 export function ProfileImage() {
   return (
     <div
       data-testid="profile-image"
-      className="bg-profile-image flex bg-cover bg-center border-4 border-cyan-600 rounded-[50%] w-52 h-52"
-    ></div>
+      className="flex border-4 border-cyan-600 rounded-[50%] w-52 h-52 overflow-hidden relative"
+    >
+      <Image
+        src="/images/profile-image.jpg"
+        alt="Foto da banda Retrôvisionários"
+        fill
+        className="object-cover"
+        priority
+      />
+    </div>
   );
 }
